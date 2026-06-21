09b5498a897f749e2c65f598242508bfbd873ad7
vickadict <egwuvictorchinonso@gmail.com>
2026-06-21 19:26:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//No 1//
+let name = 'Adaeze Jasmine';
+let myage = 27;
+let myschool = 'Baze unviersity';
+console.log(name);
+console.log(myage);
+console.log(myschool);
+
+//No 2//
+let studentName = 'Adaeze Jasmine'
+let score = 70;
+let isPresent = 'True'
+console.log(studentName);
+console.log(score);
+console.log(isPresent)
+
+//No 3//
+let price = 5000;
+let quantity = 5;
+console.log(price + quantity)
+console.log(price - quantity)
+console.log(price * quantity)
+console.log(price / quantity)
+
+//No 4//
+let firstName = "David";
+let lastName = "Peter";
+let age = 20;
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`);
+
+//No 5//
+score = 84;
+if (score >= 70) { console.log(`EXCELLENT`) }
+else if (score <= 50) { console.log(`FAIL`) }
+else { console.log(`PASS`) }
+
+//No 6//
+for (let i = 1; i <= 20; i++) { console.log(i); }
